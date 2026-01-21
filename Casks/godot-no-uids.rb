126f0df228dc17fcc25aeec3330aa7104304350a
Rafael Moterani (Rafa) <hi@rafaismy.name>
2026-01-21 00:31:14 -0500

    Update Casks/godot-no-uids.rb

---
+++ Casks/godot-no-uids.rb
@@ -0,0 +1,20 @@
+cask "godot-no-uids" do
+  version "4.5.1"
+  sha256 :no_check
+
+  url "https://github.com/rafaismyname/godot-no-uids/releases/download/#{version}-stable-nouid/godot.macos.editor.zip"
+  name "Godot Engine (No UID)"
+  desc "Game engine with UID generation disabled by default"
+  homepage "https://github.com/rafaismyname/godot-no-uids"
+
+  conflicts_with cask: "godot"
+  
+  app "Godot.app"
+  binary "#{appdir}/Godot.app/Contents/MacOS/Godot", target: "godot"
+
+  zap trash: [
+    "~/Library/Application Support/Godot",
+    "~/Library/Caches/Godot",
+    "~/Library/Saved Application State/org.godotengine.godot.savedState",
+  ]
+end
