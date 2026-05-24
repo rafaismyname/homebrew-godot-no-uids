0fdc0e1389e99331954dbaaeb278cd2ffe0aa03e
Rafael Moterani (Rafa) <hi@rafaismy.name>
2026-05-23 21:00:56 -0400

    Update godot-no-uids.rb
    4.6.3

--- Casks/godot-no-uids.rb
+++ Casks/godot-no-uids.rb
@@ -1,5 +1,5 @@
 cask "godot-no-uids" do
-  version "4.6.2"
+  version "4.6.3"
   sha256 :no_check
 
   url "https://github.com/rafaismyname/godot-no-uids/releases/download/#{version}-stable-nouid/godot.macos.editor.zip"
